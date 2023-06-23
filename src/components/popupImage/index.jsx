713158808fb3641b0415e7a35bd6422c1f2fda58
melch <melchraiol@hotmail.com>
2023-06-23 17:05:16 -0300

    first commit

---
+++ src/components/popupImage/index.jsx
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import "./styles.css"
+import Image from "../../assets/novo-caca-niquel.png"
+import XIcon from "../../assets/x-icon.png"
+import InputMask from 'react-input-mask';
+import YouTube from 'react-youtube';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+export function PopupImage({ setOpenPopupImg, openPopupVideo, openPopupImg, setOpenPopup }) {
+
+    const videoId = 'G3LZTZ-OraQ';
+    const [selectedOption, setSelectedOption] = useState('');
+    const [error, setError] = useState('');
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        gender: ''
+    });
+
+    const handleChange = (e) => {
+        setSelectedOption(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!form.name) {
+            setError('o nome é obrigatório!');
+            return;
+        }
+        if (!form.email) {
+            setError('o email é obrigatório!');
+            return;
+        }
+        if (!form.phone) {
+            setError('a número de celular é obrigatória');
+            return;
+        }
+        if (!selectedOption) {
+            setError('O gênero obrigatória');
+            return;
+        }
+        handleAddUser();
+    }
+
+    const handleAddUser = () => {
+        console.log({ form });
+
+        const dataUser = {
+            name: form.name,
+            email: form.email,
+            phone: form.phone,
+            gender: selectedOption
+        }
+
+        console.log({ dataUser });
+
+        clearForm()
+        closePopupFunction()
+        openAlert()
+    }
+
+    const clearForm = () => {
+        setError('');
+
+        setForm({
+            name: '',
+            email: '',
+            phone: '',
+            gender: ''
+        });
+    };
+
+    const handleChangeForm = (e) => {
+        const value = e.target.value;
+        setForm({ ...form, [e.target.name]: value })
+    };
+
+    const closePopupFunction = () => {
+        setOpenPopup(false)
+    }
+
+    const openAlert = () => {
+        return toast.success("CADASTRO ENVIADO COM SUCESSO", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+        });
+    }
+
+    return (
+        <>
+            <div className="container-popup">
+                <img
+                    className="x-close"
+                    src={XIcon}
+                    onClick={closePopupFunction}
+                />
+                <h1>caça-níquel</h1>
+                <h2>Venha conferir o nosso jogo</h2>
+                {openPopupImg ?
+                    <img
+                        className="imagem"
+                        src={Image}
+                    />
+                    :
+                    <div className="video">
+                        <YouTube videoId={videoId} />
+                    </div>
+                }
+                <form
+                    className="div-form"
+                    onSubmit={handleSubmit}
+                >
+                    <span>Faça o seu cadastro aqui</span>
+                    <input
+                        className={error ? 'error' : 'input'}
+                        type='text'
+                        placeholder='Nome'
+                        name='name'
+                        value={form.name}
+                        onChange={(e) => handleChangeForm(e)}
+                    />
+                    <input
+                        className={error ? 'error' : 'input'}
+                        type='text'
+                        placeholder='Email'
+                        name='email'
+                        value={form.email}
+                        onChange={(e) => handleChangeForm(e)}
+                    />
+                    <InputMask
+                        className={error ? 'error' : 'input'}
+                        mask="(99) 99999-9999"
+                        type='text'
+                        placeholder='Celular (99) 99999-9999'
+                        name='phone'
+                        value={form.phone}
+                        onChange={(e) => handleChangeForm(e)}
+                    />
+                    <select id="selectOption" className={error ? 'error' : 'div-select'} value={selectedOption} onChange={handleChange} >
+                        <option value="">Selecione o Gênero</option>
+                        <option value="feminino">Feminino</option>
+                        <option value="masculino">Masculino</option>
+                        <option value="outro">Outro</option>
+                    </select>
+                    <span className="span-error">{error}</span>
+                    <button
+                        className="btn-save"
+                    >Salvar</button>
+                </form>
+            </div>
+            <ToastContainer />
+        </>
+    )
+}
---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import "./styles.css"
+import { PopupImage } from "../../components/popupImage"
+
+export function Main() {
+
+    const [openPopup, setOpenPopup] = useState(false)
+    const [openPopupImg, setOpenPopupImg] = useState(false)
+    const [openPopupVideo, setOpenPopupVideo] = useState(false)
+
+    const handleOpenPopup = () => {
+        setOpenPopup(!openPopup)
+    }
+
+    const handleOpenImage = () => {
+        setOpenPopupImg(true)
+        setOpenPopupVideo(false)
+        handleOpenPopup()
+    }
+
+    const handlePopupVideo = () => {
+        setOpenPopupImg(false)
+        setOpenPopupVideo(true)
+        handleOpenPopup()
+    }
+
+    return (
+        <div className="container">
+            <button
+                className="btn-main"
+                onClick={() => handleOpenImage()}
+            >Popup Imagem</button>
+
+            <button
+                className="btn-main"
+                onClick={() => handlePopupVideo()}
+
+            >Popup Video</button>
+
+            {openPopup && <PopupImage
+                setOpenPopup={setOpenPopup}
+                setOpenPopupImg={setOpenPopupImg}
+                openPopupImg={openPopupImg}
+                openPopupVideo={openPopupVideo}
+            />}
+        </div>
+    )
+}
